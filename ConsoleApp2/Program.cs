a45b83dc7e8a9528e3559ee33a692e0ee0a210de
Dariusz Mędryk <dariusz.medryk@hotmail.com>
2017-11-14 00:15:13 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Affiliate.AffiliateInterfaceClient client = new Affiliate.AffiliateInterfaceClient();
+            client.authenticate("user", "klucz", true, Affiliate.Locale.pl_PL, false);
+            ////client.co
+            //var aagg = client.getAffiliateSiteTypes();
+
+            var gh = client.getMaterialIncentiveVoucherItems("291450", Affiliate.MaterialOutputType.html, new Affiliate.MaterialItemFilter { sort = Affiliate.MaterialItemSort.ID });
+            System.Console.ReadKey();
+            client.Close();
+            return;
+
+            var aaa = client.getAffiliateSites(new Affiliate.AffiliateSiteFilter { sort = Affiliate.AffiliateSiteSort.ID });
+
+            foreach (var item in aaa)
+            {
+                Console.WriteLine(item.ID + "   " + item.name + "   " + item.URL);
+            }
+
+            Affiliate.Campaign[] ggg = client.getCampaigns("291450", new Affiliate.CampaignFilter { sort = Affiliate.CampaignSort.name });
+
+            foreach (var item in ggg)
+            {
+                string output = JsonConvert.SerializeObject(item);
+
+                File.AppendAllText(string.Format(@"darek/{0}.json", item.name.Replace("/", "_")), output);
+            }
+
+            client.Close();
+            System.Console.ReadKey();
+        }
+    }
+}
